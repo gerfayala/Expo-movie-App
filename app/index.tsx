08686d3600f8c6c9c04955d7f180722c19576ac4
German <germanfayala2419@gmail.com>
2024-12-21 20:52:15 -0400

    feat: add tailwind to the project

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <View className=" flex flex-1 mt-1 justify-center ">
+      <Text className=" text-blue-800 font-bold">index</Text>
+
+      <Text className=" text-blue-800 font-bold">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
