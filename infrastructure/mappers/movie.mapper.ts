914b21941542afafe9d2afbe4830839bd2f7b441
German <germanfayala2419@gmail.com>
2024-12-30 22:04:21 -0400

    feat: add horizontal carrousel ui from TopMovies endopoint

--- infrastructure/mappers/movie.mapper.ts
+++ infrastructure/mappers/movie.mapper.ts
@@ -9,7 +9,7 @@ export class MovieMapper {
       description: movie.overview,
       releaseDate: new Date(movie.release_date),
       rating: movie.vote_average,
-      poster: `https://image.tmdb.orsg/t/p/w500${movie.poster_path}`,
+      poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
       backDrop: `https://image.tmdb.orsg/t/p/w500${movie.backdrop_path}`
     };
   };
