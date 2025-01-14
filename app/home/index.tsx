5875b9824a1474881110a213dddeeea91981e13b
German <germanfayala2419@gmail.com>
2025-01-14 05:21:37 -0400

    feat: Add scrollview for small screen size

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,4 +1,10 @@
-import {View, Text, ActivityIndicator, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  SafeAreaView,
+  ScrollView
+} from 'react-native';
 import React from 'react';
 import {useMovies} from '@/presentation/hooks/useMovies';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
@@ -20,20 +26,25 @@ const HomeScreen = () => {
   }
 
   return (
-    <View className="mt-2" style={{paddingTop: safeArea.top}}>
-      <Text className="text-2xl font-bold px-4 mb-2">Now Playing</Text>
-      <MainSlideShow movies={nowPlayingQuery.data ?? []} />
-      <MoviesHorizontalList title="Popular" movies={popularQuery.data ?? []} />
-      <MoviesHorizontalList
-        title="Top Rated"
-        movies={topRatedQuery.data?.pages.flat() ?? []}
-        loadNextPage={topRatedQuery.fetchNextPage}
-      />
-      <MoviesHorizontalList
-        title="Upcoming"
-        movies={upComingQuery.data ?? []}
-      />
-    </View>
+    <ScrollView>
+      <View className="mt-2 pb-10" style={{paddingTop: safeArea.top}}>
+        <Text className="text-2xl font-bold px-4 ">Now Playing</Text>
+        <MainSlideShow movies={nowPlayingQuery.data ?? []} />
+        <MoviesHorizontalList
+          title="Popular"
+          movies={popularQuery.data ?? []}
+        />
+        <MoviesHorizontalList
+          title="Top Rated"
+          movies={topRatedQuery.data?.pages.flat() ?? []}
+          loadNextPage={topRatedQuery.fetchNextPage}
+        />
+        <MoviesHorizontalList
+          title="Upcoming"
+          movies={upComingQuery.data ?? []}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
